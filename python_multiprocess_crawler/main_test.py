d9b9005260e55e49edb87277e35c6caad3c91fe3
Jakob Rolik <daidaloskvintus@gmail.com>
2018-10-01 19:50:45 +0200

    Testing the pool closing

--- python_multiprocess_crawler/main_test.py
+++ python_multiprocess_crawler/main_test.py
@@ -1,16 +1,15 @@
 from main import CrawlerBase
 
-# class Test(CrawlerBase):
-#     def __init__(self, *args, **kwargs) -> None:
-#         # calling the __init__ method of the CrawlerBase Class
-#         super(GoogleCrawler, self).__init__(*args, **kwargs)
+class Test(CrawlerBase):
+    def __init__(self, *args, **kwargs) -> None:
+        # calling the __init__ method of the CrawlerBase Class
+        super(Test, self).__init__(*args, **kwargs)
+    
+    def parse(self, site):
+        print(site)
+
 
 if __name__ == "__main__":
-    c = CrawlerBase()
-    p = c.downloadSourceCode('www.google.com', proxy={
-        'server': 'http://sk15.nordvpn.com',
-        'username': 'rolik.jakob@protonmail.com',
-        'password': 'mfbsitSoIaFetb46'
-    })
-
-    print(p.data)
+    c = Test()
+    c.addToQueue('www.google.com')
+    c.runMultipleProcessesUntilQueueEmpty()
